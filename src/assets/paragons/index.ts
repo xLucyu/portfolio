1742ad2cb66b31b27bd4084989cba457f090f925
Lucy <xlucyyu@gmail.com>
2025-08-07 20:59:15 +0200

    Update index.ts

---
+++ src/assets/paragons/index.ts
@@ -0,0 +1,30 @@
+import { 
+    AceMonkey, 
+    BoomerangMonkey,
+    Buccaneer,
+    DartMonkey,
+    Engineer,
+    MonkeySub,
+    NinjaMonkey,
+    SpikeFactory,
+    TackShooter,
+    WizardMonkey
+ } from "./static/index";
+import { Paragon } from "../../types/paragon";
+
+const paragons: Record<string, Paragon> = {
+  "Ace": AceMonkey,
+  "Boomerang": BoomerangMonkey,
+  "Buccaneer": Buccaneer,
+  "Dart": DartMonkey,
+  "Engineer": Engineer,
+  "Sub": MonkeySub,
+  "Ninja": NinjaMonkey,
+  "Spike": SpikeFactory,
+  "Tack": TackShooter,
+  "Wizard": WizardMonkey
+};
+
+export function getParagon(paragon: string): Paragon {
+    return paragons[paragon];
+}
