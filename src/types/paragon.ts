fb6c41e333e81d5daaa0cebd7aae9874d5abe02a
xLucyu <xlucyyu@gmail.com>
2025-08-08 00:08:10 +0200

    added types folder

---
+++ src/types/paragon.ts
@@ -0,0 +1,17 @@
+interface ParagonAttack {
+    name: string;
+    base: number;
+    ceramic?: number;
+    moab?: number;
+    boss?: number;
+    eliteBoss?: number;
+    pierce: number;
+    speed: number;
+}
+
+export interface Paragon {
+    name: string;
+    price: number;
+    image: string;
+    attacks: ParagonAttack[];
+}
