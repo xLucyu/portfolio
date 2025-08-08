8b166aeac0be464db0079d10fa0adcf752c114a3
Lucy <xlucyyu@gmail.com>
2025-08-08 16:33:46 +0000

    boomer and boat

--- src/assets/paragons/static/BoomerangMonkey.ts
+++ src/assets/paragons/static/BoomerangMonkey.ts
@@ -1,13 +1,40 @@
 export const BoomerangMonkey = {
     name: "Glaive Doominus",
-    price: 275_000,
+    price: 275000,
     image: "",
     attacks: [
         {
             name: "Glaive",
             base: 20,
+            ceramic: 40,
+            moab: 40,
+            boss: 90,
+            eliteBoss: 180,
             pierce: 100,
             speed: 0.04
+        },
+        {
+            name: "Shred",
+            base: 750,
+            eliteBoss: 1500,
+            pierce: 1
+        },
+        {
+            name: "Orbital Glaives",
+            base: 42,
+            ceramic: 62,
+            boss: 192,
+            eliteBoss: 384,
+            pierce: 1000,
+            speed: 0.1
+        },
+        {
+            name: "MOAB Press",
+            base: 1,
+            moab: 20,
+            eliteBoss: 40,
+            pierce: 100,
+            speed: 3
         }
     ]
 }
--- src/assets/paragons/static/Buccaneer.ts
+++ src/assets/paragons/static/Buccaneer.ts
@@ -1,3 +1,42 @@
 export const Buccaneer = {
-
+    name: "Navarch of the Seas",
+    price: 550000,
+    image: "",
+    attacks: [
+        {
+            name: "Plasma Turrets",
+            base: 25,
+            moab: 55,
+            boss: 85,
+            eliteBoss: 170,
+            pierce: 10,
+            speed: 0.25
+        },
+        {
+            name: "Cannonballs",
+            base: 60,
+            moab: 120,
+            boss: 180,
+            eliteBoss: 360,
+            pierce: 28,
+            speed: 0.43
+        },
+        {
+            name: "Fighter Plane Darts",
+            base: 44, 
+            boss: 88,
+            eliteBoss: 176,
+            pierce: 14,
+            speed: 0.18
+        },
+        {
+            name: "Fighter Plane Missles",
+            base: 0,
+            moab: 200,
+            boss: 400,
+            eliteBoss: 800,
+            pierce: 10,
+            speed: 1.8
+        }
+    ]
 }
